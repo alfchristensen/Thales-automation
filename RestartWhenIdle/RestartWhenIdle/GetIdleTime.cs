1b906f57bfe6a21072972e04e330215b768ab123
unknown <alf.s.christensen@gmail.com>
2017-06-10 17:06:30 +0200

    added functionality for restarting idle ticket vending machines

---
+++ RestartWhenIdle/RestartWhenIdle/GetIdleTime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RestartWhenIdle
+{ 
+    internal struct LASTINPUTINFO
+    {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
+    public class IdleTimeFinder
+    {
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetLastError();
+
+        public static uint GetIdleTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return ((uint)Environment.TickCount - lastInPut.dwTime);
+        }
+    }
+}
+
---
+++ RestartWhenIdle/RestartWhenIdle/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace RestartWhenIdle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while ((IdleTimeFinder.GetIdleTime() / 1000) < 180)
+            {
+                Console.WriteLine(IdleTimeFinder.GetIdleTime() / 1000);
+                Thread.Sleep(1000);
+            };
+
+            Console.WriteLine("Countdown completed - Killing processes");
+
+            bool processesKilled = false;
+
+            while (processesKilled == false)
+            {
+                foreach (Process proc in Process.GetProcessesByName("TVM_MMI"))
+                {
+                    proc.Kill();
+                }
+                foreach (Process proc in Process.GetProcessesByName("TVM"))
+                {
+                    proc.Kill();
+                }
+                foreach (Process proc in Process.GetProcessesByName("VCF"))
+                {
+                    proc.Kill();
+                }
+
+                Thread.Sleep(5000);
+
+                if ((Process.GetProcessesByName("TVM_MMI").Length == 0) && (Process.GetProcessesByName("TVM").Length == 0) && (Process.GetProcessesByName("VCF").Length == 0))
+                {
+                    processesKilled = true;
+                }
+                else processesKilled = false;
+            }
+
+            Console.WriteLine("Killing processes completed - restarting TVM");
+
+            System.Diagnostics.Process.Start("shutdown.exe", "-r -f");
+        }
+    }
+}
